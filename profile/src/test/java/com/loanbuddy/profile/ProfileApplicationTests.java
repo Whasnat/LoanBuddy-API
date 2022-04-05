4792cc26f51b4dd0484426336dd2353c742938de
whasnat <w.hasnat94@gmail.com>
2022-04-06 02:53:32 +0600

    Profile and Lending Engine microservice Initialized

---
+++ profile/src/test/java/com/loanbuddy/profile/ProfileApplicationTests.java
@@ -0,0 +1,13 @@
+package com.loanbuddy.profile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProfileApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
