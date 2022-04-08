3dcc8e20a01b18af8c0451fef28d67722e1468f6
whasnat <w.hasnat94@gmail.com>
2022-04-09 00:52:54 +0600

    V0.2.3

--- lendingengine/src/main/java/com/loanbuddy/lendingengine/LendingengineApplication.java
+++ lendingengine/src/main/java/com/loanbuddy/lendingengine/LendingengineApplication.java
@@ -1,5 +1,6 @@
 package com.loanbuddy.lendingengine;
 
+import com.loanbuddy.lendingengine.domain.repository.UserRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,6 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class LendingengineApplication implements CommandLineRunner {
 
+	private UserRepository userRepository;
 	public static void main(String[] args) {
 		SpringApplication.run(LendingengineApplication.class, args);
 	}
